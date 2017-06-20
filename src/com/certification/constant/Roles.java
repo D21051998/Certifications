4427147f309d7345fdb73ebfc6dca3c044c6da7c
Deepanshu Jain <ilikeitmyway1998@gmail.com>
2017-06-20 23:55:07 +0530

    Roles externalized

--- src/com/certification/constant/Roles.java
+++ src/com/certification/constant/Roles.java
@@ -2,7 +2,7 @@ package com.certification.constant;
 
 public enum Roles {
 
-	ROLE_ADMIN("ROLE_ADMIN"), ROLE_FACULTY("ROLE_FACULTY"), ROLE_STUDENT("ROLE_STUDENT");
+	ROLE_ADMIN(Messages.getString("Roles.0")), ROLE_FACULTY(Messages.getString("Roles.1")), ROLE_STUDENT(Messages.getString("Roles.2")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
 	private final String text;
 
